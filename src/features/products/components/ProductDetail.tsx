b44d06403be18b2a798874b563aecb51aaa7217e
TYE-DANG\tye-dang <tye@bradshawgroup.global>
2026-07-24 10:59:46 +0700

    [product-loading-state] add Skeleton for product detail

--- src/features/products/components/ProductDetail.tsx
+++ src/features/products/components/ProductDetail.tsx
@@ -2,8 +2,10 @@
 
 import EmptyState from "@/common/components/EmptyState/EmptyState";
 import ErrorState from "@/common/components/ErrorState/ErrorState";
-import { Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 import { useProduct } from "../hooks/useProducts";
+import ProductDetailSkeleton from "./ProductDetailSkeleton";
 
 interface Props {
   id: number;
@@ -14,7 +16,7 @@ export default function ProductDetail(props: Props) {
   const { data: product, isLoading, isError } = useProduct(id);
 
   if (isLoading) {
-    return <Typography>Loading...</Typography>;
+    return <ProductDetailSkeleton />
   }
 
   if (isError) {
@@ -26,16 +28,45 @@ export default function ProductDetail(props: Props) {
   }
 
   return (
-    <>
-      <Stack spacing={2}>
-        <Typography variant="h6">{product.title}</Typography>
+    <Stack spacing={2} sx={{ py: "20px", px: "20px" }}>
+      <Grid container spacing={2}>
+        {product.images?.[0] &&
+          <Grid
+            size={{
+              xs: 12,
+              md: 4,
+            }}
+          >
+            <Image
+              src={product.images[0]}
+              alt={product.title}
+              loading="lazy"
+              style={{
+                width: "100%",
+                height: "100%",
+                maxHeight: 420,
+                objectFit: "contain",
+              }}
+            />
+          </Grid>}
 
-        <Typography variant="h6">Price: {product.price}</Typography>
-
-        <Typography variant="h6">Category: {product.category}</Typography>
-
-        <Typography variant="h6">{product.description}</Typography>
-      </Stack>
-    </>
+        <Grid
+          size={{
+            xs: "grow",
+            md: 8,
+          }}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: 1,
+          }}>
+          <Typography variant="h6">{product.title}</Typography>
+          <Typography variant="body1">Price: {product.price}</Typography>
+          <Typography variant="body1">Category: {product.category}</Typography>
+          <Typography variant="body2">{product.description}</Typography>
+        </Grid>
+      </Grid>
+    </Stack>
   );
 }
--- src/features/products/types/product.types.ts
+++ src/features/products/types/product.types.ts
@@ -5,6 +5,7 @@ export interface Product {
   price: number;
   category: string;
   description: string;
+  images: string[];
 }
 export interface ProductListResponse {
   products: Product[];
